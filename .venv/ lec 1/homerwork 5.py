abb344017063f914ce8fbd7be4d146f0b3216c87
User <m85025278@gmail.com>
2025-08-26 22:29:44 +0300

    homework 5

---
+++ .venv/ lec 1/homerwork 5.py
@@ -0,0 +1,20 @@
+my_list=[12, 3, 4, 10]
+if len(my_list)>1:
+    last=my_list.pop()
+    my_list.insert(0,last)
+print(my_list)
+my_list=[1]
+if len(my_list)>1:
+    last=my_list.pop()
+    my_list.insert(0,last)
+print(my_list)
+my_list=[]
+if len(my_list)>1:
+    last=my_list.pop()
+    my_list.insert(0,last)
+print(my_list)
+my_list=[12, 3, 4, 10, 8]
+if len(my_list)>1:
+    last=my_list.pop()
+    my_list.insert(0,last)
+print(my_list)
