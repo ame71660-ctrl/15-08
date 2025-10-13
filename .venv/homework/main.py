cadfd8512dc57550012ed62dd73ed3021b4ca02e
ame71660-ctrl <ame71660@gmail.com>
2025-10-13 17:20:53 +0300

    Update main.py

--- .venv/homework/main.py
+++ .venv/homework/main.py
@@ -8,8 +8,8 @@ gr.add_student(st1)
 gr.add_student(st2)
 print(gr)
 
-assert gr.find_student('Jobs') == st1
-assert gr.find_student('Jobs2') is None
-assert isinstance(gr.find_student('Jobs'), Student)
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+assert isinstance(gr.find_student('Jobs'), Student) is True , 'Метод поиска должен возвращать экземпляр'
 gr.delete_student('Taylor')
 print(gr)
