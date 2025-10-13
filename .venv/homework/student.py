07b95bb4fb8cf88d5a39543ee284698eb967f4e1
ame71660-ctrl <ame71660@gmail.com>
2025-10-13 17:22:55 +0300

    Update student.py

--- .venv/homework/student.py
+++ .venv/homework/student.py
@@ -4,7 +4,6 @@ class Student(Human):
     def __init__(self, gender, age, first_name, last_name, record_book):
         super().__init__(gender, age, first_name, last_name)
         self.record_book = record_book
-
     def __str__(self):
         return f"{super().__str__()}, залікова книжка {self.record_book}"
     def __eq__(self, other):
